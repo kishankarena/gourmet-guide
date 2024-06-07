488f59d089fa3fd924cb21f00449f3b2f89c7efa
kishankarena <kishan.k@simformsolutions.com>
2024-06-07 22:32:53 +0530

    feature : Image slideshow

--- app/community/page.js
+++ app/community/page.js
@@ -1,4 +1,42 @@
-const CommunityPage = () => {
-  return <h1>Community Page</h1>;
-};
-export default CommunityPage;
+import Image from "next/image";
+
+import mealIcon from "@/assets/icons/meal.png";
+import communityIcon from "@/assets/icons/community.png";
+import eventsIcon from "@/assets/icons/events.png";
+
+import styles from "./page.module.css";
+
+export default function GourmetGuidePage() {
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>
+          Discover Culinary Delights:{" "}
+          <span className={styles.highlight}>Gourmet Guide</span>
+        </h1>
+        <p>Join our community and explore exquisite cuisines!</p>
+      </header>
+      <main className={styles.main}>
+        <h2>Community Perks</h2>
+
+        <ul className={styles.perks}>
+          <li>
+            <Image src={mealIcon} alt="A delicious meal" />
+            <p>Share & discover recipes</p>
+          </li>
+          <li>
+            <Image src={communityIcon} alt="A crowd of people, cooking" />
+            <p>Connect with fellow food enthusiasts</p>
+          </li>
+          <li>
+            <Image
+              src={eventsIcon}
+              alt="A crowd of people at a cooking event"
+            />
+            <p>Participate in exclusive culinary events</p>
+          </li>
+        </ul>
+      </main>
+    </>
+  );
+}
